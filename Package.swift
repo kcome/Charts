ee5ea12540dbf68e3b29d076cc29e4fa1a422767
Harry Li <harry@moneybrilliant.com.au>
2021-11-05 17:11:31 +1100

    Upgrade package.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Charts",
     platforms: [
-          .iOS(.v12),
+          .iOS(.v9),
           .tvOS(.v9),
           .macOS(.v10_11),
     ],
@@ -13,9 +13,19 @@ let package = Package(
         .library(
             name: "Charts",
             targets: ["Charts"]),
+        .library(
+            name: "ChartsDynamic",
+            type: .dynamic,
+            targets: ["Charts"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0")
     ],
     targets: [
-        .target(name: "Charts")
+        .target(
+            name: "Charts",
+            dependencies: [.product(name: "Algorithms", package: "swift-algorithms")]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
